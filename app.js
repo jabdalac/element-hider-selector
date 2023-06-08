19f6c85183b71c60ecd11fc47fd8787819e0a3fe
jabdalac <jairo.abdc@gmail.com>
2023-06-08 02:04:39 -0500

    First commit

---
+++ app.js
@@ -0,0 +1,9 @@
+const displayElements = () =>{
+    const datos = document.getElementById("mostrarDatos")
+    datos.style.display='block'
+}
+
+const hideElements = () =>{
+    const datos = document.getElementById("mostrarDatos")
+    datos.style.display='none'
+}
